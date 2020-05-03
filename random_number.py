8ecafc576cb41438ace0247b726c95e92a66f7b7
neha kawar shekhawat <64544435+kawarneha@users.noreply.github.com>
2020-05-03 21:04:14 +0530

    Add files via upload

---
+++ random_number.py
@@ -0,0 +1,42 @@
+import random
+
+print("how many times do u wanna play this game...?????")
+
+k=0
+
+k=int(input())
+
+for l in range(k):
+ 
+	x=0
+   
+	x=random.randrange(0,10)
+   
+	print("enter ur choice but it should be in 0 to 10")
+  
+	y=0
+   
+ 	y=int(input())
+  
+  	if(y==x):
+     
+   		print("congratulations u won!")
+  
+  	elif(y>x):
+  
+      		print("choice is greater than magic number ;(")
+   
+ 	elif(y<x):
+      
+  		print("choice is smaller than magic number oopsie!!!")
+  
+  	else:
+      
+  		print("something went wrong!!!!")
+   
+ 
+   	print("Btw magic number was")
+    
+	print (x)
+
+print("thank you:)")
